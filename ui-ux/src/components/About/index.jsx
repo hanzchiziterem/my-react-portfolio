5cbf68550138c286959156912ee345fe21967194
Hanz Chiziterem <hancarta23@gmail.com>
2025-03-15 08:29:18 +0100

    Implement the About Component

---
+++ ui-ux/src/components/About/index.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faGithub,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+            idx={15}
+          />
+        </h1>
+        <p>
+          Aspiring Full-Stack Developer with a strong foundation in front-end
+          and backend technologies. Passionate about building scalable web
+          applications, solving problems, and continiously learning new
+          technologies. Adept at working with morden frameworks and databases to
+          create seamless user experiences.
+        </p>
+        <p>
+          I'm quietly confident, naturally curious, and perpetually working on
+          improving my chops one design at a time.
+        </p>
+        <p>
+          If i need to define my self in one sentence that would be a family
+          person, father of a beautiful daughter, a sports fanatic, photography
+          enthusiast, and tech-obsessed!!!
+        </p>
+      </div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
