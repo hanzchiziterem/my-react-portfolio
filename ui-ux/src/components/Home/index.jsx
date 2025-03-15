f678f824391b222c18f0bbcb9e7a96b833f91606
Hanz Chiziterem <hancarta23@gmail.com>
2025-03-15 20:00:50 +0100

    Fix 'Page Loader' for smooth transitions through components

--- ui-ux/src/components/Home/index.jsx
+++ ui-ux/src/components/Home/index.jsx
@@ -33,42 +33,42 @@ const Home = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    },4000)
-  }, [])
+      setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
 
   return (
-<>
-<div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={Images.logoS} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Web Developer / MERN Expert / Content Writer</h2>
-        <Link to="/contact" className="flat-button">
-          Contact Me
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={Images.logoS} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Web Developer / MERN Expert / Content Writer</h2>
+          <Link to="/contact" className="flat-button">
+            Contact Me
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
-    <Loader type="pacman" />
-</>
+      <Loader type="pacman" />
+    </>
   );
 };
 
